80798ff6d61629d5662433334a53f7b134326ee2
truejasonfans <472408570@qq.com>
2023-10-03 23:31:31 +0800

    add comment for myopengl

--- myopengl.h
+++ myopengl.h
@@ -12,8 +12,26 @@ extern Matrix Viewport;
 extern Matrix Projection;
 const float depth = 2000.f;
 
+/**
+ * 视口变换矩阵
+ * @param x
+ * @param y
+ * @param w
+ * @param h
+ */
 void viewport(int x, int y, int w, int h);
+/**
+ * 投影变换矩阵
+ * @param coeff
+ */
 void projection(float coeff=0.f); // coeff = -1/c
+
+/**
+ * 模型变换
+ * @param eye
+ * @param center
+ * @param up
+ */
 void lookat(Vec3f eye, Vec3f center, Vec3f up);
 
 /**
@@ -28,10 +46,14 @@ Vec3f barycentric(Vec3i A, Vec3i B, Vec3i C, Vec3i P);
 
 struct IShader {
     virtual ~IShader() = 0;
+    // 顶点着色器
     virtual Vec4f vertex(int iface, int nthvert) = 0;
+    // 片段着色器（像素着色器）
     virtual bool fragment(Vec3f bar, TGAColor &color) = 0;
 };
 
+
+// 画三角形
 void triangle(Vec4f *pts, IShader &shader, TGAImage &image, float* zbuffer);
 template <typename T> T CLAMP(const T& value, const T& low, const T& high) {
     return value < low ? low : (value > high ? high : value);
